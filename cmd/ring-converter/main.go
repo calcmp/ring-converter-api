253841be76eb19bdf8c4e28caf0cd2e3d21b45b9
calcmp <calcmp@gmail.com>
2020-05-17 13:12:32 +0100

    Created objects, storing values in objects from request body

--- cmd/ring-converter/main.go
+++ cmd/ring-converter/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+func getConversion(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get Conversion: Hit")
+	w.Header().Set("Content-Type", "application/json")
+
+	var size Size
+
+	requestBody := json.NewDecoder(r.Body).Decode(&size)
+	if requestBody != nil {
+		log.Fatal("Error")
+		http.Error(w, requestBody.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Println(size.Millis)
+	log.Println(size.Cms)
+
+	//size, err := strconv.Atoi(requestBody)
+
+	json.NewEncoder(w).Encode(size.Millis)
+	json.NewEncoder(w).Encode(size.Cms)
+}
 
 func main() {
 	// Init router
@@ -14,8 +37,4 @@ func main() {
 	// Handle requests
 	router.HandleFunc("/api/convert", getConversion)
 	log.Fatal(http.ListenAndServe(":8081", router))
-}
-
-func getConversion(w http.ResponseWriter, r *http.Request) {
-	log.Println(convertToMillis(20))
 }
---
+++ cmd/ring-converter/ring.go
@@ -0,0 +1,20 @@
+package main
+
+// Size obj ...
+type Size struct {
+	Millis int `json:"mm,string"`
+	Cms    int `json:"cm,string"`
+}
+
+// Conversion obj ...
+type Conversion struct {
+	InsideDiameterInches int
+	InsideDiameterMillis int
+	InsideCircumMillis   int
+	SizeUSA              int
+	SizeBritish          int
+	SizeFrench           int
+	SizeGerman           int
+	SizeJapanese         int
+	SizeSwiss            int
+}
